91428def6ad5234da3ea18cc8fd1325943dbbb91
Femi Fagbure <femifagbure@gmail.com>
2020-02-13 18:44:48 -0500

    post Jack edits

--- index.js
+++ index.js
@@ -1,55 +1,71 @@
 const inquirer = require("inquirer");
 const axios = require("axios")
 const fs = require("fs");
+const path = require("path");
+const convertFactory = require("electron-html-to");
+const generateHTML = require("./generateHTML");
+const questions = [
+    {
+        type: "prompt",
+        name: "name",
+        message: "What is your full name?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your username?"
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["red", "blue", "green", "pink"]
+    }
+];
 
-inquirer
-    .prompt([
-        {
-            type: "prompt",
-            name: "name",
-            message: "What is your full name?"
-        },
-        {
-            type: "confirm",
-            name: "github",
-            message: "Do you have a github profile?"
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "What is your username?"
-        },
-        {
-            type: "input",
-            name: "color",
-            message: "What is your favorite color? (Pick from Pink, Blue, Green or Red)"
-        }
-    ])
+function writeToFile(fileName, data) {
+    console.log(fileName, data)
+}
 
-    .then(function ({ username }) {
+function init() {
+    const data = {};
+    inquirer.prompt(questions).then((answers) => {
+        //get values for data object from answers object
+        data.color = answers.color;
+        data.name = answers.name;
+
+        //get username answer to make github call
+        const username = answers.username;
         const queryUrl = `https://api.github.com/users/${username}`;
-
         axios
             .get(queryUrl)
             .then(function (res) {
-                let username = res.data.login;
-                let profile = res.data.avatar_url;
-                let location = res.data.location;
-                let github = res.data.html_url;
-                let blog = res.data.blog;
-                let bio = res.data.bio;
-                let repos = res.data.public_repos;
-                let followers = res.data.followers;
-                let following = res.data.following;
+                //get values for data object from res object
+                data.username = res.data.login;
+                data.profile = res.data.avatar_url;
+                data.location = res.data.location;
+                data.github_url = res.data.html_url;
+                data.blog = res.data.blog;
+                data.bio = res.data.bio;
+                data.repos = res.data.public_repos;
+                data.followers = res.data.followers;
+                data.following = res.data.following;
 
-console.log(username, profile, location, github, bio, blog, repos, followers, following)
-            },
-function writeToFile(fileName, data) {
+                console.log(data);
+                const htmlString = generateHTML(data);
+                console.log(htmlString);
+                //test our finished html by creating a new html file
+                fs.writeFile("./index.html", htmlString, (err) => {
+                    if (err) throw err;
 
-                },
+                    console.log("created our test html file!");
+                });
 
-function init() {
+                // console.log(generateHTML(github));
+                // console.log(generateHTML.generateHTML(github))
+                // writeToFile(generateHTML, res.data)
+            })
+    })
+};
 
-                    init()
-
-                })})
+init();
